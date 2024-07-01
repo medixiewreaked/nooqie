a45c9c4647baf4f7d5df9f040d78dd467ea817b1
dixie <mydixiewreaked@proton.me>
2024-07-01 21:24:13 +1000

    feat: adds ping
    adds example ping implementation from serenity

--- nooqie/src/main.rs
+++ nooqie/src/main.rs
@@ -1,10 +1,38 @@
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
 use std::env;
 use std::error::Error;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
     let token = env::var("DISCORD_TOKEN").expect("'DISCORD_TOKEN' environment variable not set");
-    println!("{}", token);
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+
     Ok(())
 }
