8e305eaf57bfca5e22a82420c1b8721292043c0f
dixie <mydixiewreaked@proton.me>
2024-08-11 23:03:20 +1000

    refactor: cargo fmt

--- nooqie/src/main.rs
+++ nooqie/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(deprecated)]
 
-use clap::{crate_description,Parser};
+use clap::{crate_description, Parser};
 
 use env_logger::Builder;
 
@@ -23,7 +23,13 @@ use poise::serenity_prelude as serenity;
 
 use songbird::SerenityInit;
 
-use std::{collections::{ HashMap, HashSet }, env, env::var, sync::{Arc, Mutex}, time::Duration};
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    env::var,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 mod commands;
 
@@ -31,11 +37,9 @@ use crate::commands::{ollama::*, utils::*, voice::*};
 
 use reqwest::Client as HttpClient;
 
-// Types used by all command functions
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-// Custom user data passed to all command functions
 pub struct Data {
     votes: Mutex<HashMap<String, u32>>,
 }
@@ -49,41 +53,25 @@ struct CLArgs {
 }
 
 async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
-    // This is our custom error handler
-    // They are many errors that can occur, so we only handle the ones we want to customize
-    // and forward the rest to the default handler
     match error {
         poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
         poise::FrameworkError::Command { error, ctx, .. } => {
-            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+            error!("Error in command `{}`: {:?}", ctx.command().name, error,);
         }
         error => {
             if let Err(e) = poise::builtins::on_error(error).await {
-                println!("Error while handling error: {}", e)
+                error!("Error while handling error: {}", e)
             }
         }
     }
 }
 
-// #[group]
-// #[commands(ping, llm, join, leave, play, skip, clear, pause, resume)]
-// struct General;
-
-// struct Handler;
-
-// #[async_trait]
-// impl EventHandler for Handler {
-//     async fn ready(&self, _: Context, ready: Ready) {
-//         info!("{} is connected", ready.user.name);
-//     }
-// }
-
 async fn event_handler(
     ctx: &serenity::Context,
     event: &serenity::FullEvent,
     _framework: poise::FrameworkContext<'_, Data, Error>,
-    data: &Data
-    ) -> Result<(), Error> {
+    data: &Data,
+) -> Result<(), Error> {
     match event {
         serenity::FullEvent::Ready { data_about_bot, .. } => {
             info!("{} is connected", data_about_bot.user.name);
@@ -149,60 +137,31 @@ async fn main() {
         | serenity::GatewayIntents::GUILDS
         | serenity::GatewayIntents::GUILD_VOICE_STATES;
 
-//     let framework = StandardFramework::new()
-//         .group(&GENERAL_GROUP)
-//         .help(&NOOQIE_HELP);
-//
     let prefix = match env::var("NOOQIE_PREFIX") {
-        Ok(prefix) => {
-            prefix
-        }
-        Err(_error) => {
-            String::from("!")
-        }
+        Ok(prefix) => prefix,
+        Err(_error) => String::from("!"),
     };
 
-    // FrameworkOptions contains all of poise's configuration option in one struct
-    // Every option can be omitted to use its default value
     let options = poise::FrameworkOptions {
-        // commands: vec![commands::help(), commands::vote(), commands::getvotes()],
         commands: vec![],
         prefix_options: poise::PrefixFrameworkOptions {
             prefix: Some(prefix.into()),
             edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
                 Duration::from_secs(3600),
             ))),
-            additional_prefixes: vec![
-                poise::Prefix::Literal("hey bot"),
-                poise::Prefix::Literal("hey bot,"),
-            ],
             ..Default::default()
         },
-        // The global error handler for all error cases that may occur
         on_error: |error| Box::pin(on_error(error)),
-        // This code is run before every command
         pre_command: |ctx| {
             Box::pin(async move {
                 println!("Executing command {}...", ctx.command().qualified_name);
             })
         },
-        // This code is run after a command if it was successful (returned Ok)
         post_command: |ctx| {
             Box::pin(async move {
                 println!("Executed command {}!", ctx.command().qualified_name);
             })
         },
-        // Every command invocation must pass this check to continue execution
-//         command_check: Some(|ctx| {
-//             Box::pin(async move {
-//                 if ctx.author().id == 123456789 {
-//                     return Ok(false);
-//                 }
-//                 Ok(true)
-//             })
-//         }),
-        // Enforce command checks even for owners (enforced by default)
-        // Set to true to bypass checks, which is useful for testing
         skip_checks_for_owners: false,
         event_handler: |ctx, event, framework, data| {
             Box::pin(event_handler(ctx, event, framework, data))
