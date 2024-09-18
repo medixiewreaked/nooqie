c06b65e2b0b97d2add19adce4990aacb71a96f3c
dixie <mydixiewreaked@proton.me>
2024-09-18 20:20:29 +1000

    refactor: migrates 'clear' from serenity to poise framework

--- nooqie/src/commands/voice.rs
+++ nooqie/src/commands/voice.rs
@@ -290,7 +290,7 @@ pub async fn skip(ctx: Context<'_>) -> Result<(), Error> {
 
 // #[description = "clears audio track queue"]
 #[poise::command(prefix_command, track_edits, slash_command, category = "Voice")]
-pub async fn clear(ctx: Context<'_>) -> CommandResult {
+pub async fn clear(ctx: Context<'_>) -> Result<(), Error> {
     let guild_id = {
         let guild = ctx.guild().unwrap();
         guild.id
