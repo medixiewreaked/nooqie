0383d34e58390c9e7e804159c9d7c6d544774b5c
dixie <mydixiewreaked@proton.me>
2024-10-26 17:53:12 +1100

    fix: adds handle for when 'pause' command is called outside of guild

--- nooqie/src/commands/voice.rs
+++ nooqie/src/commands/voice.rs
@@ -423,7 +423,14 @@ pub async fn pause(ctx: Context<'_>) -> Result<(), Error> {
 
     if let Some(handler_lock) = manager.get(guild_id) {
         let handler = handler_lock.lock().await;
-        let current_channel = handler.current_channel().unwrap().to_string();
+        let current_channel = match handler.current_channel() {
+            Some(channel) => channel.to_string(),
+            None => {
+                warn!("user not in voice channel, aborting");
+                return Ok(());
+            }
+        };
+
         let queue = handler.queue();
         debug!("{}: pausing audio track", current_channel);
         let _ = queue.pause();
