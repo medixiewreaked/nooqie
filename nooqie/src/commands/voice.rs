e6563a5996731530d88be8cf976b0208960c7ad2
dixie <mydixiewreaked@proton.me>
2024-10-27 20:42:43 +1100

    fix: adds handle for when 'resume' command is called outside guild

--- nooqie/src/commands/voice.rs
+++ nooqie/src/commands/voice.rs
@@ -469,7 +469,14 @@ pub async fn resume(ctx: Context<'_>) -> Result<(), Error> {
 
     if let Some(handler_lock) = manager.get(guild_id) {
         let handler = handler_lock.lock().await;
-        let current_channel = handler.current_channel().unwrap().to_string();
+        let current_channel = match handler.current_channel() {
+            Some(channel) => channel.to_string(),
+            None => {
+                warn!("user not in voice channel, aborting");
+                return Ok(());
+            }
+        };
+
         let queue = handler.queue();
         debug!("{}: resuming audio track", current_channel);
         let _ = queue.resume();
