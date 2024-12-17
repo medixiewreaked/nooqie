88fcfe0f89f937c074407f15fcd8356351ba83ec
dixie <mydixiewreaked@proton.me>
2024-12-17 17:53:21 +1100

    refactor: removes unused variable from 'skip'

--- nooqie/src/commands/voice.rs
+++ nooqie/src/commands/voice.rs
@@ -254,8 +254,8 @@ pub fn play_help() -> String {
     help_text_fn = skip_help
 )]
 pub async fn skip(ctx: Context<'_>) -> Result<(), Error> {
-    let (guild_id, connect_to) = match get_voice_info(ctx).await {
-        Ok((guild_id, connect_to)) => (guild_id, connect_to),
+    let guild_id = match get_voice_info(ctx).await {
+        Ok(info) => info.0,
         Err(err) => {
             error!("{err}");
             return Ok(());
