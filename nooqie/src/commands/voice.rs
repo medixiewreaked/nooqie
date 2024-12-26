8453590e894b70c8f2d10e8c3405ffb831f59920
dixie <mydixiewreaked@proton.me>
2024-12-26 17:12:44 +1100

    refactor: removes redundant code from 'skip'

--- nooqie/src/commands/voice.rs
+++ nooqie/src/commands/voice.rs
@@ -284,10 +284,13 @@ pub async fn skip(ctx: Context<'_>) -> Result<(), Error> {
         }
     };
 
-    let manager = songbird::get(ctx.as_ref())
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
+    let manager = match get_manager(ctx).await {
+        Ok(manager) => manager,
+        Err(err) => {
+            error!("{err}");
+            return Ok(());
+        }
+    };
 
     if let Some(handler_lock) = manager.get(guild_id) {
         let handler = handler_lock.lock().await;
